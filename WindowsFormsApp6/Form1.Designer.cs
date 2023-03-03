6cbbe4b541b85e83901ac620528971b94ee9a29b
SIISN <92276005+SIISN@users.noreply.github.com>
2023-03-03 23:50:49 +0300

    Add files via upload

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.Prev = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Volume = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "addSong";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(33, 247);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(96, 49);
+            this.Next.TabIndex = 3;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Prev
+            // 
+            this.Prev.Location = new System.Drawing.Point(33, 302);
+            this.Prev.Name = "Prev";
+            this.Prev.Size = new System.Drawing.Size(96, 49);
+            this.Prev.TabIndex = 4;
+            this.Prev.Text = "Prev";
+            this.Prev.UseVisualStyleBackColor = true;
+            this.Prev.Click += new System.EventHandler(this.Prev_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(33, 159);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(96, 44);
+            this.Pause.TabIndex = 5;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(33, 357);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(96, 42);
+            this.Delete.TabIndex = 6;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Volume
+            // 
+            this.Volume.AutoSize = true;
+            this.Volume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Volume.Location = new System.Drawing.Point(169, 367);
+            this.Volume.Name = "Volume";
+            this.Volume.Size = new System.Drawing.Size(90, 20);
+            this.Volume.TabIndex = 7;
+            this.Volume.Text = "Громкость";
+            this.Volume.Click += new System.EventHandler(this.Volume_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(173, 393);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "0:00:00";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(369, 367);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(374, 45);
+            this.trackBar2.TabIndex = 10;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(195, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(467, 95);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Volume);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Prev);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button Prev;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Label Volume;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+
+namespace WindowsFormsApp6
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Thread musicThread;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            trackBar2.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+            trackBar2.Value = Convert.ToInt32(wmp.controls.currentPosition);
+
+            if(wmp != null)
+            {
+                int s = (int)wmp.controls.currentPosition;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                label1.Text = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+            }
+            else
+            {
+                label1.Text = "0:00:00";
+            }
+        }
+
+        WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+        //WMPLib.IWMPPlaylist playlist = wmp.playlistCollection.newPlaylist("myplaylist");
+
+        WMPLib.IWMPMedia media;
+        bool pausePlay = false;
+        private void PlayMusic(string fileName)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            wmp.URL = fileName;
+            wmp.controls.play();
+            
+        }
+        DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3";
+            DoublyLinkedList<string> linkedList1 = new DoublyLinkedList<string>();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    linkedList1.Add(Path.GetFileNameWithoutExtension(fileName));
+                    linkedList.Add(fileName);
+                }
+                listBox1.Items.Clear();
+                foreach (var item in linkedList)
+                {
+                    listBox1.Items.Add(item);
+                }
+                name = linkedList.ElementAt(index);
+            }
+        }
+        string name;
+        int tindex = 0;
+        int index = 0;
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (wmp.status == "")
+                {
+                    pausePlay = true;
+                    PlayMusic(linkedList.ElementAt(index));
+
+                }
+                else if (index != tindex)
+                {
+                    pausePlay = true;
+                    wmp.controls.play();
+                   
+                }
+                else
+                {
+                    pausePlay = true;
+                    PlayMusic(linkedList.ElementAt(index));
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Добавьте треки!");
+            }
+
+            //wmp.controls.play();
+
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (pausePlay != false)
+            {
+                pausePlay = false;
+                wmp.controls.pause();
+            }
+             
+        }
+
+        private void Volume_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wmp.settings.volume = trackBar1.Value;
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        public void Delete_Click(object sender, EventArgs e)
+        {
+            linkedList.Remove(linkedList.ElementAt(index));
+            listBox1.Items.Clear();
+            foreach (var item in linkedList)
+            {
+                listBox1.Items.Add(item);
+            }
+            wmp.controls.stop();
+            tindex++;
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            tindex = index;
+            if(index == linkedList.Count() -1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+            PlayMusic(linkedList.ElementAt(index));
+        }
+
+        private void Prev_Click(object sender, EventArgs e)
+        {
+            tindex = index;
+            if(index==0)
+            {
+                index = linkedList.Count() -1;
+            }
+            else
+            {
+                index--;
+            }
+            PlayMusic(linkedList.ElementAt(index));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            index = listBox1.SelectedIndex;
+        }
+    }
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+
+    public class DoublyLinkedList<T> : IEnumerable<T>  // двусвязный список
+    {
+        DoublyNode<T> head; // головной/первый элемент
+        DoublyNode<T> tail; // последний/хвостовой элемент
+        int count;
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            // поиск удаляемого узла
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                // если узел не последний
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    // если последний, переустанавливаем tail
+                    tail = current.Previous;
+                }
+
+                // если узел не первый
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    // если первый, переустанавливаем head
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            int pos = 0;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return pos +1;
+                }
+                pos++;
+                current = current.Next;
+            }
+            return -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            DoublyNode<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+    }
+}
